ef1f6cdbc740624ab0833532141c71773b34ed42
CidVonHighwind <cid90@gmx.de>
2025-12-18 16:21:40 +0100

    moved german hyphenation test file

--- test/unit/hyphenation/EnglishHyphenationTest.cpp
+++ test/unit/hyphenation/EnglishHyphenationTest.cpp
@@ -131,7 +131,7 @@ EvaluationResult evaluateWord(const TestCase& testCase) {
 }
 
 int main(int argc, char* argv[]) {
-  std::string testDataFile = "resources/english_hyphenation_tests.txt";
+  std::string testDataFile = "test/resources/english_hyphenation_tests.txt";
 
   if (argc > 1) {
     testDataFile = argv[1];
